bbad8f1ffc3ec7111d4c61da27751ca947d5e3fb
Hem <hpmahimkar@gmail.com>
2022-04-25 22:44:04 +0530

    dashboard complete
    yay

--- client/pages/gallery/index.js
+++ client/pages/gallery/index.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Image from "next/image"
 
 const index = () => {
-  return <div>Gallery</div>;
+  return (<>
+  <div style={{textAlign:"center" , fontSize:"100px" , marginTop:"40px"}}>Gallery</div>
+
+
+    <div style={{padding:"50px"}}>
+    <Image src="/h1.jpg" height="500px" width="1600px"/>
+    </div>
+
+    
+    <div style={{padding:"50px"}}>
+    <Image src="/h2.jpg" height="500px" width="1600px"/>
+
+    </div>
+    
+    <div style={{padding:"50px"}}>
+
+    <Image src="/h3.jpg" height="500px" width="1600px"/>
+
+    </div>
+    
+    <div style={{padding:"50px"}}>
+    <Image src="/h5.jpg" height="500px" width="1600px"/>
+
+    </div>
+
+  </>
+  );
 };
 
 export default index;
